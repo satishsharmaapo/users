f73b3819efa03f69662f0cebb5315dea4adf7f7e
SATISH SHARMA <47353255+satishsharmaapo@users.noreply.github.com>
2020-10-27 04:41:28 +0530

    Create edits.js

---
+++ edits.js
@@ -0,0 +1,37 @@
+// var boxcounter;
+// $(function() {
+//     boxcounter = 0;
+//     $(".checkbox1").click(function() {
+//         if(this.checked) {
+//             counter++;
+//             if(counter == 1){
+//                 $("#editButton").attr("disabled", !this.checked);
+//             }
+//         } else {
+//             counter--;
+//             if(counter >1 || counter == 0){
+//                 $("#editButton").attr("disabled", this.checked);
+//             }
+//         }
+//     });
+// });
+ 
+var checkboxes = $("input[type='checkbox']")
+     
+
+checkboxes.click(function() {
+    
+    $(".checkbox1").click(function() {
+        if(this.checked) {
+            counter++;
+            if(counter == 1){
+                $("#editButton").attr("disabled", !checkboxes.is(":checked"));
+            }
+        } else {
+            counter--;
+            if(counter >1 || counter == 0){
+                $("#editButton").attr("disabled", !checkboxes.is(":checked"));
+            }
+        }
+    });
+});
