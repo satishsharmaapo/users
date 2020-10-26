a1ee5f5637e5f83c37b54b6c615d64fa4ae7f7ba
SATISH SHARMA <47353255+satishsharmaapo@users.noreply.github.com>
2020-10-27 03:56:25 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,28 @@
+// this js module is used for posting the form data to the json server in the form of json data
+var form = document.getElementById("form");
+form.addEventListener("submit", function (e) {
+  var username = document.getElementById("username").value;
+  var emailid = document.getElementById("emailid").value;
+   e.preventDefault();
+  //fetch post request
+  //fetch("https://my-json-server.typicode.com/satishsharmaapo/users/users/", {
+   
+   // created localhost server using npm installation of json server
+    fetch("http://localhost:3000/users/", {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      emailid: emailid,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
+
+
+fetch('http://localhost:3000/users/')
+  .then((response) => response.json())
+  .then((json) => console.log(json))
