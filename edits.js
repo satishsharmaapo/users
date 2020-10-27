2c99586c0ad696f1b3f92222324bdb21e0675799
SATISH SHARMA <47353255+satishsharmaapo@users.noreply.github.com>
2020-10-27 22:58:16 +0530

    Update edits.js

--- edits.js
+++ edits.js
@@ -1,37 +1,29 @@
-// var boxcounter;
-// $(function() {
-//     boxcounter = 0;
-//     $(".checkbox1").click(function() {
-//         if(this.checked) {
-//             counter++;
-//             if(counter == 1){
-//                 $("#editButton").attr("disabled", !this.checked);
-//             }
-//         } else {
-//             counter--;
-//             if(counter >1 || counter == 0){
-//                 $("#editButton").attr("disabled", this.checked);
-//             }
-//         }
-//     });
-// });
- 
-var checkboxes = $("input[type='checkbox']")
-     
+function editModel() {
+  var username1 = $("#username").val();
+  var emailid1 = $("#emailid").val();
+  //  formData.append('username' , username);
+  //  formData.append('emailid' , emailid);
+  //console.log(username + "  " + emailid);
 
-checkboxes.click(function() {
-    
-    $(".checkbox1").click(function() {
-        if(this.checked) {
-            counter++; 
-            if(counter == 1){
-                $("#editButton").attr("disabled", !checkboxes.is(":checked"));
-            }
-        } else {
-            counter--;
-            if(counter >1 || counter == 0){
-                $("#editButton").attr("disabled", !checkboxes.is(":checked"));
-            }
-        }
-    });
-});
+  if ($(":checkbox:checked").attr("id") != undefined) {
+    fetch("http://localhost:3000/users/" +
+        parseInt($(":checkbox:checked").attr("id")),
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          username: username1,
+          emailid: emailid1,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .catch((error) => console.error("Error:", error))
+      .then((response) => console.log("Success:", JSON.stringify(response)));
+  } else {
+    alert("Please select atmost 1 checkbox for edition");
+  }
+  location.reload();
+}
